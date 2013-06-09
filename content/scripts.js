4bf6327f547d1afd017026fe5cb60628a6868246
Jens Ameskamp <jens@ameskamp.de>
2013-06-09 10:16:19 +0200

    Upload sites using plupload. Rearranged javascript includes

--- content/scripts.js
+++ content/scripts.js
@@ -1,5 +1,5 @@
 ---
 combined: true
 ---
-- @items.select{ |i| i.identifier.start_with?('/javascript/') }.each do |i|
+- @items.select{ |i| i.identifier.start_with?('/javascript/') }.sort_by { |i| i.identifier }.each do |i|
   = i.raw_content
