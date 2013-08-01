d8b96ecd9c53354f6089569f06012bb7489fc0c9
Jens Ameskamp <jens@ameskamp.de>
2013-08-01 17:14:22 +0200

    Upload functionality implemented

--- Rakefile
+++ Rakefile
@@ -61,10 +61,7 @@ def update(image_directory, id)
   index_file = File.join(content_directory, id, 'index.yaml')
   abort "File #{index_file} does not exist" unless File.exists?(index_file)
 
-  if Dir[id_directory + '/*/'].any?
-    puts "Updating folder #{id}" if verbose == true
-    update_folder(id_directory, index_file)
-  else
+  if not Dir[id_directory + '/*/'].any?
     puts "Updating album #{id}" if verbose == true
     update_album(id_directory, index_file)
   end
@@ -92,44 +89,20 @@ def update_album(album_directory, index_file)
   puts "Updated album #{album_directory}. Total: #{total}. New: #{total - old_count}" 
 end
 
-def update_folder(folder_directory, index_file)
-  index = YAML.load_file(index_file)
-  index['children'] = [] if index['children'].nil?
- 
-  existing_children = get_subdirectories(folder_directory)
-
-  index['children'].reject! { |i| existing_children.index { |ei| ei['id'] == i['id'] }.nil? } 
-
-  last = index['children'].max_by { |i| i['sort_order'] } 
-  max = 0
-  max = last['sort_order'] unless last.nil?
-
-  existing_children.select { |ei| index['children'].index { |i| i['id'] == ei['id'] }.nil? }.each do |new_child|
-    new_child['sort_order'] = max + 1
-    max = max + 1
-    index['children'] << new_child
-  end
-
-  index['children'].sort! { |a,b| a['sort_order'] <=> b['sort_order'] }
-  File.open(index_file, 'w+') do |out|
-    YAML.dump(index, out)
-  end
-end
-
 
 def get_images(directory)
   Dir.glob(directory + '/*.jpg', File::FNM_CASEFOLD).collect { |i| { 'id' => File.basename(i), 'date' => nil } }
 end
 
-def get_subdirectories(directory)
-  Dir.glob(directory + '/*/').collect { |d| { 'id' => File.basename(d), 'sort_order' => nil } }
-end
-
 def get_date(image)
-  date = EXIFR::JPEG.new(image).date_time.inspect
+  date = EXIFR::JPEG.new(image).date_time
   if date.nil?
-    date = File.mtime(image).inspect
+    date = File.mtime(image)
   end
+  if date.nil?
+    date = DateTime.now
+  end
+  date.inspect
 end
 
 def get_directory(path)
--- static/upload_helper.php
+++ static/upload_helper.php
@@ -1,30 +1,9 @@
 <?php
 
-//
-// To see the PHP example in action, please do the following steps.
-//
-// 1. Open test/js/uploader-demo-jquery.js file and change the request.endpoint
-// parameter to point to this file.
-//
-//  ...
-//  request: {
-//    endpoint: "../server/php/example.php"
-//  }
-//  ...
-//
-// 2. As a next step, make uploads and chunks folders writable.
-//
-// 3. Open test/jquery.html to see if everything is working correctly,
-// the uploaded files should be going into uploads folder.
-//
-// 4. If the upload failed for any reason, please open the JavaScript console,
-// if this does not help please read the excellent documentation we have for you.
-//
-// https://github.com/Widen/fine-uploader/blob/master/readme.md
-//
-
 // Include the uploader class
 require_once 'qqFileUploader.php';
+
+header("Content-Type: application/json");
 
 $uploader = new qqFileUploader();
 
@@ -38,10 +17,19 @@ $uploader->sizeLimit = 20 * 1024 * 1024;
 $uploader->inputName = 'qqfile';
 
 // If you want to use resume feature for uploader, specify the folder to save parts.
-$uploader->chunksFolder = 'chunks';
+$uploader->chunksFolder = '/tmp';
+
+
+if(!isset($_POST['album']))
+    exit(json_encode(array('error' => 'Kein Album angegeben.')));
+
+$target_folder = '/srv/images/originals/' . $_POST['album'];
+
+if(!is_dir($target_folder))
+    exit(json_encode(array('error' => 'Das Albumverzeichnis '.$target_folder.' wurde nicht gefunden.')));
 
 // Call handleUpload() with the name of the folder, relative to PHP's getcwd()
-$result = $uploader->handleUpload('/srv/nanoc-gallery/uploads');
+$result = $uploader->handleUpload($target_folder);
 
 // To save the upload with a specified name, set the second parameter.
 // $result = $uploader->handleUpload('uploads/', md5(mt_rand()).'_'.$uploader->getName());
@@ -49,5 +37,4 @@ $result = $uploader->handleUpload('/srv/nanoc-gallery/uploads');
 // To return a name used for uploaded file you can use the following line.
 $result['uploadName'] = $uploader->getUploadName();
 
-header("Content-Type: text/plain");
 echo json_encode($result);
